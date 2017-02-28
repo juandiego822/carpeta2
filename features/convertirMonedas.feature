9a5f4ea874670b5852403ba60a6a18d07d8dd1c8
juandiego822 <juandiego822@gmail.com>
2017-02-28 17:22:48 -0500

    proyecto_inicial

---
+++ features/convertirMonedas.feature
@@ -0,0 +1,39 @@
+#Author: your.email@your.domain.com
+#Keywords Summary :
+#Feature: List of scenarios.
+#Scenario: Business rule through list of steps with arguments.
+#Given: Some precondition step
+#When: Some key actions
+#Then: To observe outcomes or validation
+#And,But: To enumerate more Given,When,Then steps
+#Scenario Outline: List of steps for data-driven as an Examples and <placeholder>
+#Examples: Container for s table
+#Background: List of steps run before each of the scenarios
+#""" (Doc Strings)
+#| (Data Tables)
+#@ (Tags/Labels):To group Scenarios
+#<> (placeholder)
+#""
+## (Comments)
+#Sample Feature Definition Template
+@tag
+Feature: Conversor de monedas universal
+  I want to use this template for my feature file
+
+  @tag1
+  Scenario: convertir dolares a pesos
+    Given que quiero convertir dolares en pesos
+    When convierto 100 dolares en pesos
+    Then el resultado es 300000 pesos
+
+  @tag2
+  Scenario: convertir dolares a pesos con parametros
+    Given que quiero convertir dolares en pesos
+    When convierto "100" dolares en pesos
+    Then el resultado es "100,00 USD = 287.324,85 COP"
+
+  @tag5
+  Scenario Outline: persona con sobrepeso
+    Given que estoy en la calculadora de Indice de Masa Corporal
+    When Ingreso los valores de peso y altura de una persona con sobrepeso
+    Then la calculadora indica que está en sobrepeso
---
+++ src/test/java/StepDefinition/PruebaCucumber.java
@@ -0,0 +1,67 @@
+package StepDefinition;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+
+public class PruebaCucumber {
+	
+	WebDriver driver;
+	String resultado;
+	
+	@Given("^que quiero convertir dolares en pesos$")
+	public void que_quiero_convertir_dolares_en_pesos() throws Throwable {
+	    
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://themoneyconverter.com/ES/USD/COP.aspx");
+		driver.manage().window().maximize();
+		
+	}
+
+	@When("^convierto 100 dolares en pesos$")
+	public void convierto_dolares_en_pesos() throws Throwable {
+	    
+		driver.findElement(By.id("ta")).sendKeys("100");
+		
+	}
+
+	
+	@Then("^el resultado es 300000 pesos$")
+	public void el_resultado_el_pesos() throws Throwable {
+		resultado = driver.findElement(By.id("res1")).getText();
+		System.out.println(resultado);
+		//Assert.assertEquals("COP/USD = 2.865,2", driver.findElement(By.id("res1")).getText());
+		assertEquals("100,00 USD = 287.324,85 COP", resultado);
+		driver.close();
+			
+	}
+
+	@When("^convierto \"([^\"]*)\" dolares en pesos$")
+	public void convierto_dolares_en_pesos(String arg1) throws Throwable {
+		
+		driver.findElement(By.id("ta")).sendKeys(arg1);
+		
+	}
+
+	@Then("^el resultado es \"([^\"]*)\"$")
+	public void el_resultado_es(String arg1) throws Throwable {
+	    
+		resultado = driver.findElement(By.id("res1")).getText();
+		System.out.println(resultado);
+		//Assert.assertEquals("COP/USD = 2.865,2", driver.findElement(By.id("res1")).getText());
+		assertEquals(arg1, resultado);
+		driver.close();
+		
+	}
+
+}
